9e296100d24389cdd1f1ac38d9b2d7fd922c2520
mantissa89 <128544361+mantissa89@users.noreply.github.com>
2024-01-09 23:19:35 +0000

    Add start screen

--- ConvertinatorBeta.java
+++ ConvertinatorBeta.java
@@ -1,2 +1,13 @@
+import java.util.*;
 public class ConvertinatorBeta {
+    public static void main(String[] args) {
+        System.out.println("\n░░      ░░░      ░░   ░░░  ░  ░░░░  ░        ░       ░░        ░        ░   ░░░  ░░      ░░        ░░      ░░       ░░\n" +
+                "▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒    ▒▒  ▒  ▒▒▒▒  ▒  ▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒  ▒▒▒▒    ▒▒  ▒  ▒▒▒▒  ▒▒▒▒  ▒▒▒▒  ▒▒▒▒  ▒  ▒▒▒▒  ▒\n" +
+                "▓  ▓▓▓▓▓▓▓  ▓▓▓▓  ▓  ▓  ▓  ▓▓  ▓▓  ▓▓      ▓▓▓       ▓▓▓▓▓  ▓▓▓▓▓▓▓  ▓▓▓▓  ▓  ▓  ▓  ▓▓▓▓  ▓▓▓▓  ▓▓▓▓  ▓▓▓▓  ▓       ▓▓\n" +
+                "█  ████  █  ████  █  ██    ███    ███  ███████  ███  █████  ███████  ████  ██    █        ████  ████  ████  █  ███  ██\n" +
+                "██      ███      ██  ███   ████  ████        █  ████  ████  ████        █  ███   █  ████  ████  █████      ██  ████  █\n");
+        System.out.println("Welcome to the Convert-inator!\n");
+        System.out.print("What type of units would you like to convert today?");
+        String unitClass = input.next();
+    }
 }
